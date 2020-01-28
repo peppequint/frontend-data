1f865c4b6a05ba95547b4a6bd0eb66c4cfa32e38
Peppe-Quint <peppequintb@gmail.com>
2020-01-28 16:51:28 +0100

    add donut (not done yet)

--- docs/scripts/bundle.js
+++ docs/scripts/bundle.js
@@ -97153,9 +97153,9 @@ let data = require('../../data/data.json');
 const select = ['Overall', 'During World War I', 'Post World War I', 'During World War II', 'Post World War II'];
 
 const margin = 30;
-const height = 920 - margin;
-const width = 1420 - margin;
-
+const height = 800 - margin;
+const width = 1000 - margin;
+  
 let svg = d3
   .select('.container')
   .append('svg')
@@ -97172,6 +97172,15 @@ const y = d3.scaleLinear();
 
 const t = d3.transition().duration(200);
 
+const renderBarChart = () => {
+  data = modifyData.barChart();
+
+  data.sort((a, b) => d3.ascending(a.key, b.key));
+
+  options();
+  scaleBarChart(data);
+};
+
 const updateBarChart = data => {
   graph.remove();
   graph = svg
@@ -97180,15 +97189,6 @@ const updateBarChart = data => {
     .attr('class', 'graph');
   console.log(data);
 
-  scaleBarChart(data);
-};
-
-const createVisualisation = () => {
-  data = modifyData.barChart();
-
-  data.sort((a, b) => d3.ascending(a.key, b.key));
-
-  options();
   scaleBarChart(data);
 };
 
@@ -97227,7 +97227,7 @@ const scaleBarChart = data => {
   let max = d3.max(data, d => +d.value);
 
   x.range([0, width - 20]).domain(data.map(d => d.key));
-  y.range([height, 0]).domain([0, max + 20]);
+  y.range([height, 0]).domain([0, max + 10]);
   axisBarChart(data);
 };
 
@@ -97257,13 +97257,13 @@ const axisBarChart = data => {
   graph
     .append('text')
     .attr('x', width / 2 + margin)
-    .attr('y', margin * 10.5 * 3)
+    .attr('y', height + margin * 2)
     .text('Jaren');
 
-  drawBarChart(data);
+  drawBars(data);
 };
 
-const drawBarChart = data => {
+const drawBars = data => {
   const bars = graph
     .selectAll()
     .data(data)
@@ -97282,10 +97282,13 @@ const drawBarChart = data => {
   bars.on('mouseenter', function(value) {
     d3.select(this)
       .append('text')
+      .style('opacity', '0')
       .attr('x', v => x(v.key) + 13)
       .attr('y', v => y(v.value) - 13)
-      // .attr('text-anchor', 'middle')
+      .attr('text-anchor', 'middle')
       .text(v => `${v.value}`)
+      .transition(t)
+      .style('opacity', '1')
       .attr('class', 'tooltip-value');
   });
 
@@ -97299,8 +97302,6 @@ const drawBarChart = data => {
 function changeOption() {
   switch (this.value) {
     case 'Overall':
-      console.log(data);
-
       updateBarChart(data.filter(y => y.key > 1910 && y.key < 1951));
       break;
     case 'Post World War II':
@@ -97320,8 +97321,90 @@ function changeOption() {
   }
 }
 
-createVisualisation();
+const widthDonut = 450;
+const heightDonut = 450;
+const marginDonut = 40;
 
+let dataDonut = {};
+
+function makeDonut(i) {
+  const transformData = () => {
+    return d3
+      .nest()
+      .key(d => d.year)
+      .rollup(d => d)
+      .entries(data);
+  };
+
+  let string;
+  let year = transformData();
+
+  year.map(data => {
+    if (data.key === i) {
+      string = data;
+    }
+  });
+
+  let total = string.value;
+  let array = [];
+
+  total.map(d => array.push(d.title));
+  let dataString = array.join();
+
+  const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const parseData = {};
+
+  letters.forEach(letter => {
+    let uppercase = letter.toUpperCase();
+
+    let amountLowerCase = dataString.split(letter).length - 1;
+    let amountUpperCase = dataString.split(uppercase).length - 1;
+
+    parseData[letter] = amountLowerCase + amountUpperCase;
+  });
+
+  console.log(parseData);
+
+  dataDonut = parseData;
+}
+
+makeDonut('1930');
+
+const radiusDonut = Math.min(widthDonut, heightDonut) / 2 - marginDonut;
+
+let svgDonut = d3
+  .select('.container')
+  .append('svg')
+  .attr('width', widthDonut)
+  .attr('height', heightDonut)
+  .append('g')
+  .attr('transform', `translate(${widthDonut / 2}, ${heightDonut / 2})`);
+
+const color = d3.scaleOrdinal(d3.schemePastel2).domain(dataDonut);
+
+const pie = d3
+  .pie()
+  .value(d => d.value)
+
+const data_ready = pie(d3.entries(dataDonut))
+
+svgDonut
+  .selectAll()
+  .data(data_ready)
+  .enter()
+  .append('path')
+  .attr(
+    'd',
+    d3
+      .arc()
+      .innerRadius(125)
+      .outerRadius(radiusDonut)
+  )
+  .attr('fill', d => color(d.data.key))
+  // .style('opacity', 0.7);
+
+
+  renderBarChart();
 },{"../../data/data.json":1,"d3":34}],3:[function(require,module,exports){
 // https://d3js.org/d3-array/ v1.2.4 Copyright 2018 Mike Bostock
 (function (global, factory) {
